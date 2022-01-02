8d99c020bcd889f2c466b7853a4624fc6ad0506b
raihan6714 <ab40096@gmail.com>
2022-01-02 19:54:18 +0600

    added Subscribe in about page

--- src/Components/Pages/About/Subscribe/Subscribe.js
+++ src/Components/Pages/About/Subscribe/Subscribe.js
@@ -3,7 +3,26 @@ import React from 'react';
 const Subscribe = () => {
     return (
         <>
-            
+            <section className="py-6 py-md-11 border-top" data-jarallax data-speed=".8" style={{backgroundImage: "url(assets/img/illustrations/illustration-1.jpg)"}}>
+                <div className="container text-center py-xl-4">
+                    <div className="row">
+                        <div className="col-xl-7 mx-auto">
+                            <h1 className="text-capitalize">Subscribe our newsletter</h1>
+                            <p className="text-capitalize font-size-lg mb-md-6 mb-4">Your download should start automatically, if not Click here. Should I give up, huh?.</p>
+                            <div className="mx-md-8">
+                                <form>
+                                    <div className="input-group">
+                                        <input type="text" className="form-control" placeholder="Enter your email" aria-label="Enter your email" aria-describedby="button-addon2"/>
+                                        <div className="input-group-append">
+                                            <button className="btn btn-primary btn-sm-wide" type="button" id="button-addon2">Subscribe</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </>
     );
 };
