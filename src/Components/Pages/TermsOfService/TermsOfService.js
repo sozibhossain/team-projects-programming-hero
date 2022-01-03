2568e2660295315f396696d6625473ecf874e1cc
sozibhossain <sozibhossain69@gmail.com>
2022-01-03 08:31:45 +0600

    blog-termsofservice

--- src/Components/Pages/TermsOfService/TermsOfService.js
+++ src/Components/Pages/TermsOfService/TermsOfService.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const TermsOfService = () => {
     return (
         <div>
+            <div class="py-8 py-md-11">
+                <div class="container text-center py-xl-2">
+                    <h1 class="display-4 fw-semi-bold mb-0">Terms of Service</h1>
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb breadcrumb-scroll justify-content-center">
+                            <li class="breadcrumb-item">
+                                <NavLink className="nav-link px-xl-4" to="/"  id="navbarLandings">
+                                    Home
+                                </NavLink>
+                            </li>
+                            <li class="breadcrumb-item">
+                                <NavLink className="nav-link px-xl-4" to="/termsofservice"  id="navbarLandings">
+                                Terms of Service
+                                </NavLink>
+                            </li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+
+
             <div>
                 <div class="container mb-11">
                     <div class="row">
